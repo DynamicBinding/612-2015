4235f7f8f4aa861da42538c2f24e1eb6534a25f2
Ahmad Bamba <ahmad.eb.bamba@gmail.com>
2015-01-28 14:50:00 -0500

    initial practice board commit

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -15,9 +15,7 @@ void Robot::RobotInit()
 	CommandBase::init();
 	autonomousCommand = new ExampleCommand();
 	lw = LiveWindow::GetInstance();
-	robotDrive = new MecanumDrivetrain(MOTOR_LF,MOTOR_LR,MOTOR_RF,MOTOR_RR);//The 4 talons
 	joystick = new Joystick(1);//Right hand joystick
-	speedgun = new BuiltInAccelerometer(); // New accelerometer called speedgun
 	robot = this;
 }
 
@@ -38,8 +36,7 @@ void Robot::DisabledPeriodic()
 
 void Robot::AutonomousInit()
 {
-	if (autonomousCommand != NULL)
-		autonomousCommand->Start();
+
 }
 
 void Robot::AutonomousPeriodic()
@@ -49,40 +46,12 @@ void Robot::AutonomousPeriodic()
 
 void Robot::TeleopInit()
 {
-	// This makes sure that the autonomous stops running when
-	// teleop starts running. If you want the autonomous to
-	// continue until interrupted by another command, remove
-	// this line or comment it out.
-	if (autonomousCommand != NULL)
-		autonomousCommand->Cancel();
+
 }
 
 void Robot::TeleopPeriodic()
 {
 	Scheduler::GetInstance()->Run();
-
-	static unsigned int TimeChecked = 0;
-	TimeChecked++;
-	currentAcceleration = (speedgun -> GetY())*9.806; // covert from g force to acceleration
-	if (currentAcceleration > maxAcceleration)// check to see if currentAcceleration is bigger that maxAcceleration. if yes, set maxAcceleration to currentAcceleration
-	{
-		maxAcceleration = currentAcceleration; //set the values
-	}
-	if (TimeChecked == 30) //print every half second
-	{
-		lw->Run();
-		float val = joystick->GetRawAxis(5);//Takes input from joystick
-		float leftYAxis = joystick->GetRawAxis(2);
-		firstTalon->Set(val);//Gives joystick input to first talon
-		secondTalon->Set(val);
-		thirdTalon->Set(leftYAxis);
-	}
-
-	printf("Raw G-force on Y-axis is %f meters per second per second \n", speedgun -> GetY()); //prints raw g-force
-	printf("Acceleration is %f meters per second per second \n", currentAcceleration); //prints currentAcceleration
-	printf("Max acceleration is %f meters per second per second \n", maxAcceleration); //prints maxAcceleration
-	TimeChecked = 0;
-
 }
 
 void Robot::TestInit()
@@ -92,10 +61,6 @@ void Robot::TestInit()
 void Robot::TestPeriodic()
 {
 	lw->Run();
-	float val = joystick->GetRawAxis(5);//Takes input from joystick
-	firstTalon->Set(val);//Gives joystick input to first talon
-	secondTalon->Set(val);
-	
 }
 
 
--- src/Robot.h
+++ src/Robot.h
@@ -1,20 +1,11 @@
 #ifndef ROBOT_H
 #define ROBOT_H
-#include "Subsystems/MecanumDrivetrain.h"
 
 class Robot: public IterativeRobot
 {
 private:
-	Command* autonomousCommand;
 	LiveWindow* lw;
 	Joystick* joystick;
-	Talon* firstTalon;
-	Talon* secondTalon;
-	Talon* thirdTalon;
-	//all acceleration is measured in meters per second squared
-	BuiltInAccelerometer* speedgun; // Used for speedgun, a accelerometer
-	double currentAcceleration = 0; // used for acceleration and accelerometer
-	double maxAcceleration = 0; // used for highest acceleration during total run time
 	Robot* robot;
 public:
 	void RobotInit();
@@ -26,10 +17,5 @@ public:
 	void TestPeriodic();
 	void DisabledInit();
 	void DisabledPeriodic();
-
-	//612 functions
-	void TestPulley();
-	//612 Objects
-	MecanumDrivetrain* robotDrive;
 };
 #endif
